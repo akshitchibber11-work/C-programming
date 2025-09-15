4472365d48c91626e00878ed07406834fdc74227
Akshit Chibber <akshitchibber11@gmail.com>
2025-09-15 09:20:10 +0530

    Add files via upload

---
+++ exp2ques1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+     int length,width,area,perimeter;
+     printf("akshit\n");
+     printf("enter length and width\n");
+     scanf("%d %d",&length, &width);
+     area=length*width;
+     perimeter=2*(length+width);
+     printf("Area will be %d\n",area);
+     printf("perimeter will be %d\n",perimeter);
+     return 0;
+}
---
+++ exp2ques2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+     float celcius,farenheit;
+     printf("akshit\n");
+     printf("enter temp in celcius\n");
+     scanf("%f",&celcius);
+     farenheit=(celcius*(9/5)+32);
+     printf("temp in farenheit is %f",farenheit);
+     return 0;
+}
